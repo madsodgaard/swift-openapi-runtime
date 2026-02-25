13e69bf1cfeffc46053cea24544cae4a5d5887ac
Mads Odgaard <mads.odgaard@frameo.com>
2026-02-25 10:07:12 +0100

    refactor `computeSafeString`

--- Sources/OpenAPIRuntime/URICoder/Serialization/URISerializer.swift
+++ Sources/OpenAPIRuntime/URICoder/Serialization/URISerializer.swift
@@ -50,22 +50,6 @@ struct URISerializer {
     }
 }
 
-extension CharacterSet {
-
-    /// A character set of unreserved symbols only from RFC 6570 (excludes
-    /// alphanumeric characters).
-    fileprivate static let unreservedSymbols: CharacterSet = .init(charactersIn: "-._~")
-
-    /// A character set of unreserved characters from RFC 6570.
-    fileprivate static let unreserved: CharacterSet = .alphanumerics.union(unreservedSymbols)
-
-    /// A character set with only the space character.
-    fileprivate static let space: CharacterSet = .init(charactersIn: " ")
-
-    /// A character set of unreserved characters and a space.
-    fileprivate static let unreservedAndSpace: CharacterSet = .unreserved.union(space)
-}
-
 extension URISerializer {
 
     /// A serializer error.
@@ -109,12 +93,33 @@ extension URISerializer {
         // The space character needs to be encoded based on the config,
         // so first allow it to be unescaped, and then we'll do a second
         // pass and only encode the space based on the config.
-        let partiallyEncoded = unsafeString.addingPercentEncoding(withAllowedCharacters: .unreservedAndSpace) ?? ""
-        let fullyEncoded = partiallyEncoded.replacingOccurrences(
-            of: " ",
-            with: configuration.spaceEscapingCharacter.rawValue
-        )
-        return fullyEncoded
+        let spaceReplacement = configuration.spaceEscapingCharacter.rawValue
+        let spaceReplacementBytes = spaceReplacement.utf8
+
+        let percent = UInt8(ascii: "%")
+        let space = UInt8(ascii: " ")
+        let utf8Buffer = unsafeString.utf8
+        let maxLength = utf8Buffer.count * 3
+        return withUnsafeTemporaryAllocation(of: UInt8.self, capacity: maxLength) { outputBuffer in
+            var i = 0
+            for byte in unsafeString.utf8 {
+                if byte == space {
+                    for spaceReplacementByte in spaceReplacementBytes {
+                        outputBuffer[i] = spaceReplacementByte
+                        i += 1
+                    }
+                } else if byte.isUnreserved {
+                    outputBuffer[i] = byte
+                    i += 1
+                } else {
+                    outputBuffer[i] = percent
+                    outputBuffer[i+1] = hexToAscii(byte >> 4)
+                    outputBuffer[i+2] = hexToAscii(byte & 0xF)
+                    i += 3
+                }
+            }
+            return String(decoding: outputBuffer[..<i], as: UTF8.self)
+        }
     }
 
     /// Provides a raw string value for the provided key.
@@ -320,6 +325,47 @@ extension URICoderConfiguration {
         switch (style, explode) {
         case (.form, false): return "="
         default: return nil
+        }
+    }
+}
+
+@inline(__always)
+fileprivate func hexToAscii(_ hex: UInt8) -> UInt8 {
+    switch hex {
+    case 0x0: return UInt8(ascii: "0")
+    case 0x1: return UInt8(ascii: "1")
+    case 0x2: return UInt8(ascii: "2")
+    case 0x3: return UInt8(ascii: "3")
+    case 0x4: return UInt8(ascii: "4")
+    case 0x5: return UInt8(ascii: "5")
+    case 0x6: return UInt8(ascii: "6")
+    case 0x7: return UInt8(ascii: "7")
+    case 0x8: return UInt8(ascii: "8")
+    case 0x9: return UInt8(ascii: "9")
+    case 0xA: return UInt8(ascii: "A")
+    case 0xB: return UInt8(ascii: "B")
+    case 0xC: return UInt8(ascii: "C")
+    case 0xD: return UInt8(ascii: "D")
+    case 0xE: return UInt8(ascii: "E")
+    case 0xF: return UInt8(ascii: "F")
+    default: fatalError("Invalid hex digit: \(hex)")
+    }
+}
+
+extension UInt8 {
+    /// Checks if a byte is an unreserved character per RFC 3986.
+    fileprivate var isUnreserved: Bool {
+        switch self {
+        case UInt8(ascii: "0")...UInt8(ascii: "9"),
+            UInt8(ascii: "A")...UInt8(ascii: "Z"),
+            UInt8(ascii: "a")...UInt8(ascii: "z"),
+            UInt8(ascii: "-"),
+            UInt8(ascii: "."),
+            UInt8(ascii: "_"),
+            UInt8(ascii: "~"):
+            return true
+        default:
+            return false
         }
     }
 }
